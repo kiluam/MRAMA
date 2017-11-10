41ab4eba8db01f060bb2b7e5c2fa57ddc8c0fd60
kiluam <smaulik@gmail.com>
2017-11-10 10:58:30 +0000

    GridsearchCV added

--- mrama_script.py
+++ mrama_script.py
@@ -8,8 +8,8 @@ from sklearn.model_selection import train_test_split
 
 def main():
 	attributeVar = ['sLength', 'sWidth', 'pLength', 'pWidth' , 'flowerType']
-	df = pd.read_table("http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data", delimiter = ',', names = attributeVar)
-	# df = pd.read_table("iris.data.txt", delimiter = ',', names = attributeVar)
+	# df = pd.read_table("http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data", delimiter = ',', names = attributeVar)
+	df = pd.read_table("iris.data.txt", delimiter = ',', names = attributeVar)
 	# df = df.sample(frac=1).reset_index(drop=True)	
 	y = df['flowerType'].astype('category').cat.codes
 	y = y.tolist()
@@ -18,18 +18,18 @@ def main():
 		df[columns] = df[columns]/df[columns].max()
 	x = df.as_matrix()
 	x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.3)
-	clf = mr.Mrama(l_rate = 0.5, K = 15, L = 100)
-	scores = clf.cross_validation(x_train, y_train, cv=5)
-	print scores #cross-validation accuracy in %
-	model = clf.fit(x_train, y_train)
-	y_pred = clf.predict(x_test)
+	# clf = mr.Mrama()
+	scores = mr.Mrama.cross_validation(x_train, y_train, 5, 10, niters = [50, 100, 200, 500],l_rate = [0.1, 0.2, 0.3], K = [15, 100], L = [100, 1000])
+	# print scores #cross-validation accuracy in %
+	# model = clf.fit(x_train, y_train)
+	# y_pred = clf.predict(x_test)
 
-	test_accuracy = 0
-	for i,j in it.izip(y_test, y_pred):
-		if i == j:
-			test_accuracy += 1
+	# test_accuracy = 0
+	# for i,j in it.izip(y_test, y_pred):
+	# 	if i == j:
+	# 		test_accuracy += 1
 
-	print test_accuracy #Absolute Accuracy
+	# print test_accuracy #Absolute Accuracy
 
 
 if __name__=="__main__":
